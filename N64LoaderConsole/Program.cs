e52e1f6dca4153edeb72bd64113591178b941a0f
Robin Jones <robin@networkfusion.co.uk>
2016-04-04 19:46:13 +0100

    Code refactoring and simplification in an attempt to get roms to pass checksum.

--- N64LoaderConsole/Program.cs
+++ N64LoaderConsole/Program.cs
@@ -23,23 +23,19 @@ namespace N64LoaderConsole
             Console.WriteLine("**********************************");
             if (args.Length != 0 && !string.IsNullOrEmpty(args[0]))
             {
-                var connected = InitialiseSerialPort();
-
-                if (connected)
+                if (InitialiseSerialPort())
                 {
                     Console.WriteLine("Writing ROM...");
                     //todo: detect file type is correct, if not convert
-                    WriteRom(args[0]);
+                    WriteRom(args[0].ToString());
 
-
+                    Thread.Sleep(1000); //TODO: testing to see if the rom image is not ready, remove if possible!
                     var startPacket = new CommandPacket(CommandPacket.Command.StartRom, 508);
-                    startPacket.Send(IoPort);
-
-                    IoPort.Close();
+                    startPacket.Send(IoPort);                    
                 }
                 else
                 {
-                    Console.WriteLine("ED64's usb port not detected");
+                    Console.WriteLine("No response received");
                 }
 
             }
@@ -49,8 +45,21 @@ namespace N64LoaderConsole
                 Console.WriteLine(@"No ROM specified, e.g. ""loader.exe c:\mycart.v64"".");
             }
 
-            Console.WriteLine(@"Press any key to exit.");
+            if (IoPort.IsOpen)
+            {
+                IoPort.Close();
+            }
+
+            Console.WriteLine("Press any key to exit.");
             Console.ReadLine();
+        }
+
+        ~Program()
+        {
+            if (IoPort.IsOpen)
+            {
+                IoPort.Close();
+            }
         }
 
         private static bool InitialiseSerialPort()
@@ -75,21 +84,15 @@ namespace N64LoaderConsole
                     IoPort.Read(receiveBuffer, 0 , 512); //should be 4 if not 512
                     //ReadResponse(port, 4); //expect RSPk
 
-                    if (receiveBuffer[3] != 107) //k
+                    if (receiveBuffer[3] == 107) //k
                     {
-                        IoPort.Close();
-                        //throw new Exception("no response form " + device.nodeDescription);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Connected to EverDrive64 on port: " + device.nodeComportName);
+                        Console.WriteLine("Connected to EverDrive64 on port: {0}", device.nodeComportName);
                         return true;
                     }
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("error: " + ex.ToString());
-                    Console.ReadLine();
+                    Console.WriteLine("error: {0}", ex.ToString());
                 }
             }
             return false;
@@ -97,23 +100,24 @@ namespace N64LoaderConsole
 
         public static void WriteRom(string fileName)
         {
-            int fileLength;
+            int fileLength = 0;
             byte[] fileArray;
 
             using (FileStream fileStream = File.OpenRead(fileName))
             {
                 fileLength = (int)fileStream.Length;
-                if ((long)(fileLength / 65536 * 65536) != fileStream.Length)
+                if ((long)(fileLength / 0x10000 * 0x10000) != fileStream.Length) //65536
                 {
-                    fileLength = (int)(fileStream.Length / 65536L * 65536L + 65536L);
+                    fileLength = (int)(fileStream.Length / 0x10000 * 0x10000 + 0x10000);
                 }
                 fileArray = new byte[fileLength];
                 fileStream.Read(fileArray, 0, (int)fileStream.Length);
             }
             Console.WriteLine("File Size: " + fileLength);
 
-            if (fileLength < 2097152) //0x200000 //needs filling
+            if (fileLength < 0x200000) //2097152 //needs filling
             {
+                Console.WriteLine("Generating space for ROM");
                 var fillPacket = new CommandPacket(CommandPacket.Command.Fill, 508);
                 fillPacket.Send(IoPort);
 
@@ -122,12 +126,12 @@ namespace N64LoaderConsole
 
                 if (receiveBuffer[3] != 107)
                 {
-                    Console.WriteLine("fill error");
+                    Console.WriteLine("Error generating space required, exiting");
                     return;
                 }
                 else
                 {
-                    Console.WriteLine("fill ok");
+                    Console.WriteLine("ROM space generated");
                 }
             }
 
@@ -142,36 +146,38 @@ namespace N64LoaderConsole
             writePacket.body(commandInfo);
             writePacket.Send(IoPort);
 
-            Console.WriteLine("sending...");
+            Console.WriteLine("Sending ROM...");
             DateTime now = DateTime.Now;
-            for (int l = 0; l < fileArray.Length; l += 32768)
+            for (int l = 0; l < fileArray.Length; l += 0x8000) //32768 (256KB)
             {
-                if (l == 33554432)
+                if (l == 0x2000000) // 33554432 (32MB)
                 {
-                    Console.WriteLine("next 32m!!!!!!!!!!!!!!!");
+                    Console.WriteLine("Sending next 32MB chunk");
 
-                    commandInfo[0] = 64; //0x40 - 32mb offset
+                    commandInfo[0] = 0x40; //64 - 32MB offset
                     commandInfo[1] = 0;
-                    commandInfo[2] = (byte)((fileArray.Length - 33554432) / 512 >> 8);
-                    commandInfo[3] = (byte)((fileArray.Length - 33554432) / 512);
+                    commandInfo[2] = (byte)((fileArray.Length - 0x2000000) / 512 >> 8);
+                    commandInfo[3] = (byte)((fileArray.Length - 0x2000000) / 512);
                     writePacket.body(commandInfo);
                     writePacket.Send(IoPort);
                 }
-                IoPort.Write(fileArray, l, 32768);
-                if (l % 524288 == 0)
+
+                IoPort.Write(fileArray, l, 0x8000); //32768 (256KB)
+
+                if (l % 0x80000 == 0) //524288 (512KB)
                 {
-                    Console.WriteLine("sent: " + l);
+                    Console.WriteLine("sent 512KB chunk: {0} of {1}", l, fileArray.Length);
                 }
             }
-            Console.WriteLine("sent: " + fileArray.Length);
-            Console.WriteLine("time: " + (DateTime.Now - now).Ticks / 10000L);
+            Console.WriteLine("sent file: {0}", fileArray.Length);
+            Console.WriteLine("elapsed time: {0}", (DateTime.Now - now).Ticks / 10000L);
 
-            ushort crc = 0;
-            foreach (byte b in fileArray)
-            {
-                crc += (ushort)b;
-            }
-            Console.WriteLine("crc: " + crc);
+            //ushort crc = 0;
+            //foreach (byte b in fileArray)
+            //{
+            //    crc += (ushort)b;
+            //}
+            //Console.WriteLine("crc: " + crc);
         }
 
         private static void ReadResponse(Stream stream, int length)
--- RomConverter/RomConverter.cs
+++ RomConverter/RomConverter.cs
@@ -8,24 +8,25 @@ using System.Threading.Tasks;
 
 
 //based on https://raw.githubusercontent.com/masl123/n64RomConverter/master/com/github/masl123/n64RomConverter/Program.java
+// good resource http://jul.rustedlogic.net/thread.php?id=11769
 
 namespace RomConverter
 {
     public class RomConverter
     {
-        public enum OutputType
+        public enum RomType : uint
         {
-            n64,
-            v64,
-            z64
+            n64 = 0x40123780,
+            v64 = 0x37804012,
+            z64 = 0x80371240
         }
 
-        public async Task ConvertFile(string InFilePath, OutputType OutFileType)
+        public async Task ConvertFile(string InFilePath, RomType OutFileType)
         {
             await ConvertFile(InFilePath, new FileInfo(InFilePath).DirectoryName, OutFileType);
         }
 
-        public async Task ConvertFile(string InFilePath, string OutDirPath, OutputType OutFileType)
+        public async Task ConvertFile(string InFilePath, string OutDirPath, RomType OutFileType)
         {
             Debug.WriteLine("Converting n64 Roms from [n64,v64,z64] to [n64,v64,z64]");
             try {
@@ -53,27 +54,30 @@ namespace RomConverter
 
                             //get file header type
                             infs.Seek(0, SeekOrigin.Begin);
-                            byte[] headerArray = new byte[4];
-                            infs.(headerArray, 0, 4);
-                            uint RomHeader = BitConverter.ToUInt32(headerArray, 0);
+
+                            RomType romHeader = 0;
+                            using (BinaryReader reader = new System.IO.BinaryReader(infs))
+                            {
+                                romHeader = (RomType)reader.ReadUInt32();
+                            }
+
 
                             infs.Seek(0, SeekOrigin.Begin);
 
                             //get the right format of the original ROM
-                            switch (RomHeader)
+                            switch (romHeader)
                             {
 
-                                case 0x40123780:
-                                    //n64
+                                case RomType.n64:
                                     switch (OutFileType)
                                     {
-                                        case OutputType.n64:
+                                        case RomType.n64:
                                             //nothing to do here
                                             break;
-                                        case OutputType.v64:
+                                        case RomType.v64:
                                             wordSwap(dWordSwap(inBuffer));
                                             break;
-                                        case OutputType.z64:
+                                        case RomType.z64:
                                             dWordSwap(inBuffer);
                                             break;
                                         default:
@@ -81,17 +85,16 @@ namespace RomConverter
                                     }
                                     break;
 
-                                case 0x80371240:
-                                    //z64
+                                case RomType.z64:
                                     switch (OutFileType)
                                     {
-                                        case OutputType.n64:
+                                        case RomType.n64:
                                             dWordSwap(inBuffer);
                                             break;
-                                        case OutputType.v64:
+                                        case RomType.v64:
                                             wordSwap(inBuffer);
                                             break;
-                                        case OutputType.z64:
+                                        case RomType.z64:
                                             //nothing to do here
                                             break;
                                         default:
@@ -99,17 +102,16 @@ namespace RomConverter
                                     }
                                     break;
 
-                                case 0x37804012:
-                                    //v64
+                                case RomType.v64:
                                     switch (OutFileType)
                                     {
-                                        case OutputType.n64:
+                                        case RomType.n64:
                                             dWordSwap(wordSwap(inBuffer));
                                             break;
-                                        case OutputType.v64:
+                                        case RomType.v64:
                                             //nothing to do here
                                             break;
-                                        case OutputType.z64:
+                                        case RomType.z64:
                                             wordSwap(inBuffer);
                                             break;
                                         default:
